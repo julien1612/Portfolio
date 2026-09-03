f5152a28dfa8ea0f65b8b0f7daa42e34c2ab0303
julien1612 <cjulien1612@gmail.com>
2026-09-03 23:19:06 +0200

    modif header

--- var/cache/dev/twig/bc/bc0311641be6e1634693bdd8e960663c.php
+++ var/cache/dev/twig/bc/bc0311641be6e1634693bdd8e960663c.php
@@ -46,15 +46,13 @@ class __TwigTemplate_e76c1009fb739159f452110cbbc62d3e extends Template
 
         // line 1
         yield "<nav class=\"navbar navbar-expand-lg navbar-dark color-nav shadow-sm\">
-\t<div class=\"container-fluid\">
-\t\t<a class=\"navbar-brand d-flex align-items-center\" href=";
-        // line 3
-        yield ">
-\t\t\t<img src=\"";
+
+\t<div class=\"container-fluid \">
+\t\t<img src=\"";
         // line 4
-        yield "\" alt=\"Logo\" class=\"logo-header\">
-\t\t\t<h1 class=\"titleH1 mb-0 ms-3 fs-4 fs-lg-2 display-lg-6\">Le Bonheur des Petits Guerriers</h1>
-\t\t</a>
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("pictures/photo_moi.PNG"), "html", null, true);
+        yield "\" alt=\"Julien Chassin\" class=\"logo-header ms-3 \">
+\t\t<h1 class=\"titleH1 mb-0 ms-3 ms-lg-5 fs-4 fs-lg-2 display-lg-6\">CHASSIN Julien<br>Développeur Full Stack</h1>
 
 \t\t<button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarLBPG\">
 \t\t\t<span class=\"navbar-toggler-icon\"></span>
@@ -64,30 +62,27 @@ class __TwigTemplate_e76c1009fb739159f452110cbbc62d3e extends Template
 \t\t\t<ul class=\"navbar-nav ms-auto text-center gap-2 mt-3 mt-lg-0\">
 \t\t\t\t<li class=\"nav-item\">
 \t\t\t\t\t<a class=\"nav-link color-link ";
-        // line 15
-        yield "\">L'association</a>
+        // line 14
+        yield "\">À propos</a>
 \t\t\t\t</li>
 \t\t\t\t<li class=\"nav-item\">
 \t\t\t\t\t<a class=\"nav-link color-link ";
-        // line 18
-        yield "\">Actualités</a>
+        // line 17
+        yield "\">Projets</a>
 \t\t\t\t</li>
 \t\t\t\t<li class=\"nav-item\">
 \t\t\t\t\t<a class=\"nav-link color-link ";
-        // line 21
+        // line 20
+        yield "\">Compétences</a>
+\t\t\t\t</li>
+\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t<a class=\"nav-link color-link me-3\" ";
+        // line 23
         yield "\">Contact</a>
 \t\t\t\t</li>
-\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t<a class=\"nav-link color-link ";
-        // line 24
-        yield "\">Nous soutenir</a>
-\t\t\t\t</li>
-\t\t\t\t<li class=\"nav-item d-flex align-items-center\">
-\t\t\t\t\t<a class=\"nav-link nav-btn-soutenir px-4 ms-lg-3\" href=\"https://www.helloasso.com/associations/le-bonheur-des-petits-guerriers\">
-\t\t\t\t\t\tFaire un don
-\t\t\t\t\t</a>
-\t\t\t\t</li>
-\t\t\t</ul>
+\t\t\t\t";
+        // line 30
+        yield "\t\t\t</ul>
 \t\t</div>
 \t</div>
 </nav>
@@ -112,19 +107,26 @@ class __TwigTemplate_e76c1009fb739159f452110cbbc62d3e extends Template
     /**
      * @codeCoverageIgnore
      */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
     public function getDebugInfo(): array
     {
-        return array (  83 => 24,  78 => 21,  73 => 18,  68 => 15,  55 => 4,  52 => 3,  48 => 1,);
+        return array (  85 => 30,  81 => 23,  76 => 20,  71 => 17,  66 => 14,  53 => 4,  48 => 1,);
     }
 
     public function getSourceContext(): Source
     {
         return new Source("<nav class=\"navbar navbar-expand-lg navbar-dark color-nav shadow-sm\">
-\t<div class=\"container-fluid\">
-\t\t<a class=\"navbar-brand d-flex align-items-center\" href={#{{ path('app_home') }}#}>
-\t\t\t<img src=\"{#{{ asset('pictures/logo.webp') }}#}\" alt=\"Logo\" class=\"logo-header\">
-\t\t\t<h1 class=\"titleH1 mb-0 ms-3 fs-4 fs-lg-2 display-lg-6\">Le Bonheur des Petits Guerriers</h1>
-\t\t</a>
+
+\t<div class=\"container-fluid \">
+\t\t<img src=\"{{ asset('pictures/photo_moi.PNG') }}\" alt=\"Julien Chassin\" class=\"logo-header ms-3 \">
+\t\t<h1 class=\"titleH1 mb-0 ms-3 ms-lg-5 fs-4 fs-lg-2 display-lg-6\">CHASSIN Julien<br>Développeur Full Stack</h1>
 
 \t\t<button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarLBPG\">
 \t\t\t<span class=\"navbar-toggler-icon\"></span>
@@ -133,22 +135,22 @@ class __TwigTemplate_e76c1009fb739159f452110cbbc62d3e extends Template
 \t\t<div class=\"collapse navbar-collapse\" id=\"navbarLBPG\">
 \t\t\t<ul class=\"navbar-nav ms-auto text-center gap-2 mt-3 mt-lg-0\">
 \t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_home' ? 'active' : '' }}\" href=\"{{ path('app_home') }}#}\">L'association</a>
+\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_home' ? 'active' : '' }}\" href=\"{{ path('app_home') }}#}\">À propos</a>
 \t\t\t\t</li>
 \t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_article' ? 'active' : '' }}\" href=\"{{ path('app_article') }}#}\">Actualités</a>
+\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_article' ? 'active' : '' }}\" href=\"{{ path('app_article') }}#}\">Projets</a>
 \t\t\t\t</li>
 \t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_contact' ? 'active' : '' }}\" href=\"{{ path('app_contact') }}#}\">Contact</a>
+\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_contact' ? 'active' : '' }}\" href=\"{{ path('app_contact') }}#}\">Compétences</a>
 \t\t\t\t</li>
 \t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t<a class=\"nav-link color-link {#{{ app.request.get('_route') == 'app_soutenir' ? 'active' : '' }}\" href=\"{{ path('app_soutenir') }}#}\">Nous soutenir</a>
+\t\t\t\t\t<a class=\"nav-link color-link me-3\" {#{{ app.request.get('_route') == 'app_soutenir' ? 'active' : '' }}\" href=\"{{ path('app_soutenir') }}#}\">Contact</a>
 \t\t\t\t</li>
-\t\t\t\t<li class=\"nav-item d-flex align-items-center\">
+\t\t\t\t{#<li class=\"nav-item d-flex align-items-center\">
 \t\t\t\t\t<a class=\"nav-link nav-btn-soutenir px-4 ms-lg-3\" href=\"https://www.helloasso.com/associations/le-bonheur-des-petits-guerriers\">
-\t\t\t\t\t\tFaire un don
+\t\t\t\t\t\tMon CV
 \t\t\t\t\t</a>
-\t\t\t\t</li>
+\t\t\t\t</li>#}
 \t\t\t</ul>
 \t\t</div>
 \t</div>
